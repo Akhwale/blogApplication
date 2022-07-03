51f44684c5fcb3630a2b46a47be967b9be342508
ELLY AKWOYO <iamopetu@gmail.com>
2022-07-03 18:40:41 +0300

    The commits on post crude

---
+++ learning_blog/app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Admin\PostFormRequest;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts =Post::all();
+        return view('admin.post.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('admin.post.create');
+    }
+
+    public function store(PostFormRequest $request)
+    {
+        $data = $request->validated();
+
+        $post = new Post;
+        $post->name = $data['name'];
+        $post->slug = $data['slug'];
+        $post->description = $data['description'];
+        $post->yt_frame = $data['yt_frame'];
+        $post->meta_title = $data['meta_title'];
+        $post->meta_description = $data['meta_description'];
+        $post->meta_keyword = $data['meta_keyword'];
+        $post->status = $request -> status == true? '1':'0';
+        $post->created_by = Auth::user()->id;
+        $post->save();  
+        
+        return redirect('admin/posts') -> with('message','Post Added Successfuly!');
+    }
+
+    public function edit($post_id)
+    {
+        $post = Post::find($post_id);
+        return view('admin.post.edit', compact('post'));
+    }
+
+    public function update(PostFormRequest $request, $post_id)
+    {
+        $data = $request->validated();
+
+        $post = Post::find($post_id);
+        $post->name = $data['name'];
+        $post->slug = $data['slug'];
+        $post->description = $data['description'];
+        $post->yt_frame = $data['yt_frame'];
+        $post->meta_title = $data['meta_title'];
+        $post->meta_description = $data['meta_description'];
+        $post->meta_keyword = $data['meta_keyword'];
+        $post->status = $request -> status == true? '1':'0';
+        $post->created_by = Auth::user()->id;
+        $post->update();  
+        
+        return redirect('admin/posts') -> with('message','Post Updated Successfuly!');
+    }
+
+    public function destroy($post_id)
+    {
+        $post = Post::find($post_id);
+        $post->delete();
+
+        return redirect('admin/posts') -> with('message','Post deleted Successfuly!');
+    }
+}
--- learning_blog/resources/views/layouts/master.blade.php
+++ learning_blog/resources/views/layouts/master.blade.php
@@ -19,6 +19,11 @@
     <!-- Styles -->
     <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+
+    <!--summernote css link-->
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+
 </head>
 <body>
 
@@ -34,9 +39,23 @@
                     @include('layouts.inc.admin-footer')
             </div>
     </div>
-
+    jquery-3.6.0.min
+    <script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/js/scripts.js') }}"></script>
-    
+     
+    <!--summernote js link-->
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $("#mysummernote").summernote({
+                height:200,
+            });
+
+            $('.dropdown-toggle').dropdown();
+        });
+    </script>
+
 </body>
 </html>
